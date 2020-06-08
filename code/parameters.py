18e68f81cc0dcf9006042f12ee4179516aeb64d0
Gaurav Ray <thegman19951@gmail.com>
2020-06-08 16:07:19 +0100

    small commit

--- code/parameters.py
+++ code/parameters.py
@@ -1,21 +1,22 @@
 # PARAMETERS FOR corffitter
 
-TDATA = range(48)
+TDATA = range(96)
 TFIT  = TDATA[2:16]
 TP    = 48
-NEXP  = range(1,7)
-s_coeff = (-1, -1)
+NEXP  = range(1,10)
+s_coeff = (1, 1)
 
 OSC     = True
 CORRFIT = True
-PLOT    = True
+PLOT    = False 
 SAVEFIG = False
 
-dir = 'proc_corrs/ape4/'
+dir = '../data/proc_corrs/l3296f211b630m0074m037m440-coul-v5/'
 # l3248f211b580m002426m06730m8447
+# l3296f211b630m0074m037m440-coul-v5
 
-name = 'hybrid_lrho_4ape.txt'
-key         = '1-+_4ape_lrho'    # for fitting, doesn't affect plotting
+name = 't0_Hy_m0.450.txt'
+key  = 'pion.ll'    # for fitting, doesn't affect plotting
 
 filename_in = dir + name
 
